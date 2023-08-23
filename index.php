43d8b3bd76b01cc2b0ce778441ca77f5de6b62aa
wilsm06 <williamsm.06@hotmail.com>
2023-08-23 20:20:40 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <title>Home - SosVias</title>
+</head>
+<body>
+    <?php require_once("includes/header.php") ?>
+    <main>
+        <div class="movie_home"></div>
+        <div class="sobre_home"></div>
+        <div class="tutorial_home"></div>
+    </main>
+</body>
+</html>
