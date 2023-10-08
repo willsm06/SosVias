7ed38ab0e14d08aaca3e32044a5eb696a452af7d
willsm06 <williamsm.06@hotmail.com>
2023-10-08 19:03:11 -0300

    criaçao home

--- index.php
+++ index.php
@@ -1,17 +1,61 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Home - SosVias</title>
 </head>
+
 <body>
     <?php require_once("includes/header.php") ?>
     <main>
-        <div class="movie_home"></div>
-        <div class="sobre_home"></div>
-        <div class="tutorial_home"></div>
+        <div class="initial_wrapper">
+            <div class="initial_left_content">
+                RESPOSTAS <br>
+                RÁPIDAS VIAS <br>
+                MAIS SEGURAS
+            </div>
+            <div class="initial_right_content">
+                <img src="assets/img/logo_sos.png" style="height: 100%;" alt="logo sos">
+            </div>
+            <img src="assets/img/via_detail.png" alt="rodovia" class="rodovia_detail">
+        </div>
+        <div class="second_wrapper">
+            <div class="second_left_content">
+                <h2>QUEM SOMOS</h2>
+                Lorem Ipsum is simply dummy
+                text of the printing and typesetting
+                industry. Lorem Ipsum has been the
+                industry's standard dummy text ever
+                since the 1500s, when an unknown
+                printer took a galley of type and
+                scrambled it to make.
+            </div>
+            <div class="second_right_content">
+                <img src="assets/img/van_detail.png" style="height: 100%;" alt="logo sos">
+            </div>
+            <img src="assets/img/street_detail.png" class="street_detail" alt="street detail">
+        </div>
+        <div class="info_wrapper">
+
+        </div>
+        <div class="app_wrapper">
+            <img src="assets/img/app_hand.png" alt="hand">
+            <div class="content_app">
+                <h2>CONHEÇA NOSSO APP</h2>
+                Lorem Ipsum is simply dummy
+                text of the printing and typesetting
+                industry. Lorem Ipsum has been the
+                industry's standard dummy text ever
+                since the 1500s, when an unknown
+                printer took a galley of type and
+                scrambled it to make.
+                <a href=""><button>Saiba Mais</button></a>
+            </div>
+        </div>
     </main>
 </body>
+
 </html>
